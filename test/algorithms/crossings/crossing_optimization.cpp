3f9afe8020e63aee465ba4eeb71bb4ac7030d956
Marcel Walter <marcel.walter@tum.de>
2022-08-26 15:22:08 +0200

    Added tests for crossing optimization with level skipping and with blocks, respectively

--- test/algorithms/crossings/crossing_optimization.cpp
+++ test/algorithms/crossings/crossing_optimization.cpp
@@ -88,3 +88,88 @@ TEMPLATE_TEST_CASE(
   // optimization should not be able to reduce the number of crossings in this instance
   CHECK( straight_line_crossings( rank_ntk ) == 4 );
 }
+
+TEMPLATE_TEST_CASE(
+    "Crossing optimization with fanins in different levels", "[crossings]",
+    aig_network, mig_network, xag_network, xmg_network, klut_network, cover_network )
+{
+  rank_view<depth_view<fanout_view<TestType>>> rank_ntk{};
+
+  // rank 1
+  auto const x1 = rank_ntk.create_pi();
+  auto const x2 = rank_ntk.create_pi();
+
+  // rank 2
+  auto const x3 = rank_ntk.create_and( x1, x2 );
+  auto const x4 = rank_ntk.create_or( x1, x2 );
+
+  // rank 3
+  auto const x5 = rank_ntk.create_and( x3, x2 );
+  auto const x6 = rank_ntk.create_and( x4, x1 );
+
+  // outputs
+  rank_ntk.create_po( x5 );
+  rank_ntk.create_po( x6 );
+
+  // validate rank positions
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x1 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x2 ) ) == 1 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x3 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x4 ) ) == 1 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x5 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x6 ) ) == 1 );
+
+  // 4 crossings prior to optimization
+  REQUIRE( straight_line_crossings( rank_ntk ) == 4 );
+
+  crossing_optimization( rank_ntk );
+
+  // 1 crossing after
+  CHECK( straight_line_crossings( rank_ntk ) == 1 );
+}
+
+TEMPLATE_TEST_CASE(
+    "Crossing optimization with blocks", "[crossings]",
+    klut_network, cover_network )
+{
+  rank_view<depth_view<fanout_view<TestType>>> rank_ntk{};
+
+  // rank 1
+  auto const x1 = rank_ntk.create_pi(); // 2
+  auto const x2 = rank_ntk.create_pi(); // 3
+  auto const x3 = rank_ntk.create_pi(); // 4
+
+  // rank 2
+  const auto n1 = rank_ntk.create_not( x1 );     // 5
+  const auto n2 = rank_ntk.create_not( x2 );     // 6
+  auto const x4 = rank_ntk.create_and( x2, x3 ); // 7
+
+  // rank 3
+  const auto n22 = rank_ntk.create_not( n2 );    // 8
+  auto const x5 = rank_ntk.create_and( n1, x4 ); // 9
+
+  // rank 4
+  auto const x6 = rank_ntk.create_and( n22, x5 ); // 10
+
+  // output
+  rank_ntk.create_po( x6 );
+
+  // validate rank positions
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x1 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x2 ) ) == 1 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x3 ) ) == 2 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( n1 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( n2 ) ) == 1 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x4 ) ) == 2 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( n22 ) ) == 0 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x5 ) ) == 1 );
+  REQUIRE( rank_ntk.rank_position( rank_ntk.get_node( x6 ) ) == 0 );
+
+  // 1 crossing prior to optimization
+  REQUIRE( straight_line_crossings( rank_ntk ) == 1 );
+
+  crossing_optimization( rank_ntk );
+
+  // 0 crossings after
+  CHECK( straight_line_crossings( rank_ntk ) == 0 );
+}
